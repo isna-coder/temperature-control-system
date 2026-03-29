00b27a749b8bd67c9a22b24603403ee6ac715881
isna <isnahk06@gmail.com>
2026-03-29 11:31:03 +0530

    Updated project with auto and manual modes

--- temp_control_system.ino
+++ temp_control_system.ino
@@ -1,3 +1,6 @@
+char input;
+char mode = 'A';   
+
 int sensorPin = A0;
 int led = 7;
 
@@ -7,19 +10,49 @@ void setup() {
 }
 
 void loop() {
-  int value = analogRead(sensorPin);
 
-  float voltage = value * (5.0 / 1023.0);
-  float temperature = (voltage - 0.5) * 100;
+ 
+  if (Serial.available()) {
+    input = Serial.read();
 
-  Serial.print("Temperature: ");
-  Serial.println(temperature);
-
-  if (temperature > 40) {
-    digitalWrite(led, HIGH);
-  } else {
-    digitalWrite(led, LOW);
+    if (input == 'M') {
+      mode = 'M';
+      Serial.println("Manual Mode");
+    }
+    else if (input == 'A') {
+      mode = 'A';
+      Serial.println("Auto Mode");
+    }
+    else if (mode == 'M') {
+      if (input == '1') {
+        digitalWrite(led, HIGH);
+        Serial.println("LED ON");
+      }
+      else if (input == '0') {
+        digitalWrite(led, LOW);
+        Serial.println("LED OFF");
+      }
+    }
   }
 
-  delay(1000);
+  
+  if (mode == 'A') {
+
+    int reading = analogRead(sensorPin);
+
+    float voltage = reading * (5.0 / 1023.0);
+    float temperature = (voltage - 0.5) * 100;
+
+    Serial.print("Temp: ");
+    Serial.println(temperature);
+
+    if (temperature > 40) {
+      digitalWrite(led, HIGH);
+    } 
+    else {
+      digitalWrite(led, LOW);
+    }
+  }
+
+  delay(500);
 }
